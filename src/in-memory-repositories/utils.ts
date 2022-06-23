a00434496761ad860015135f16a356f3f87f64f7
Kevin <kevin@uninorte.edu.co>
2022-06-22 21:29:46 -0500

    changes in pipeline

--- src/in-memory-repositories/utils.ts
+++ src/in-memory-repositories/utils.ts
@@ -3,13 +3,13 @@ import { writeFile } from "fs/promises"
 import path from "path"
 
 export function readJson<T>(dbName: string): T[] {
-  const dbPath = path.join(__dirname, "json-backup", dbName)
+  const dbPath = path.join(__dirname, "../../json-db-backup", dbName)
   const db: T[] = JSON.parse(readFileSync(dbPath, { encoding: "utf-8" }) || "[]")
   return db
 }
 
 export function saveJson<T>(dbName: string, entries: T[]): Promise<void> {
-  const dbPath = path.join(__dirname, "json-backup", dbName)
+  const dbPath = path.join(__dirname, "../../json-db-backup", dbName)
   const json = JSON.stringify(entries, null, 2) + "\n"
   return writeFile(dbPath, json)
 }
